f992ae104462ec809bf02ee64442f1c37e80e355
Ruthless <tsb27development@gmail.com>
2022-04-11 11:25:06 +0100

    ex1

---
+++ Aviao/Aviao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Aviao
+{
+    public class Aviao
+    {
+        private string modelo;
+        private float velocidade;
+        private float altitude;
+        private string marca;
+
+
+
+        public void Reduzir()
+        {
+            Console.WriteLine("reduzir");
+        }
+
+
+
+        public void Descer()
+        {
+            Console.WriteLine("descer");
+        }
+
+
+
+        public void Acelerar()
+        {
+            Console.WriteLine("acelerar");
+        }
+
+
+
+        public string Subir()
+        {
+            return "subir --->";
+        }
+    }
+}
